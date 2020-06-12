874194ae01288aaba0012ebd5085d1a8a5be1c1f
andrey30353 <andrey30353@mail.ru>
2020-06-12 15:06:11 +0300

    Кривой маршрут и движение по нему

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,20 @@
+﻿using BansheeGz.BGSpline.Curve;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public BGCurve Route;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using BansheeGz.BGSpline.Curve;
+using UnityEngine;
+
+public enum Route
+{
+    Left = -1,
+    Center = 0,
+    Right = 1,
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public BGCurve Route;
+    public GameObject Model;
+
+    [Range(0.01f , 10f)]
+    public float ChangeRouteSpeed = 1;
+
+    int desiredRoute;
+
+    private void Awake()
+    {
+        desiredRoute =(int) Model.transform.localPosition.x;
+    }
+
+    private void Update()
+    {   
+        // направо - внутрь круга
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            desiredRoute = desiredRoute + 1;
+
+            //if (Model.transform.localPosition.x < 1)
+            //    Model.transform.localPosition += Vector3.right;
+        }
+
+        // налево - наружу круга
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            desiredRoute = desiredRoute - 1;
+
+            //if (Model.transform.localPosition.x > -1)
+            //    Model.transform.localPosition += Vector3.left;
+        }
+        Move(desiredRoute);
+    }
+
+    private void Move(int desiredRoute)
+    {
+        var currentRoute = Model.transform.localPosition.x;
+
+        desiredRoute = Mathf.Clamp(desiredRoute, -1, 1);
+
+        if (Mathf.Approximately(currentRoute, desiredRoute))
+            return;
+
+        var moveX = 0f;
+
+        if (desiredRoute > currentRoute)
+            moveX = 1 * ChangeRouteSpeed * Time.deltaTime;
+        if (desiredRoute < currentRoute)
+            moveX = -1 * ChangeRouteSpeed * Time.deltaTime;
+
+
+        var newPositionX = Mathf.Clamp(Model.transform.localPosition.x + moveX, -1, 1);
+        // округляем около центрального маршрута
+        if (desiredRoute == 0 && newPositionX > -0.1f && newPositionX < 0.1f)
+        {
+            newPositionX = 0f;
+        }
+
+        Model.transform.localPosition = new Vector3(newPositionX, Model.transform.localPosition.y, Model.transform.localPosition.z);
+    }
+}
