927146b12e8519286985150fdc7592a2c75bcc50
andrey30353 <andrey30353@mail.ru>
2020-06-12 19:01:00 +0300

    Добавил шар на цепи. Добавил модель рабочего. Перенес скрипты в папку

---
+++ Assets/Scripts/BallChain.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BallChain : MonoBehaviour
+{   
+    public GameObject Ball;
+    
+    public float speed = 5f; //скорость туда-сюда
+    public float amp = 30; //величина размаха
+
+    private LineRenderer lineRender;
+
+    [ContextMenu("Нарисовать веревку")]
+    private void ShowLine()
+    {
+        if (Ball == null)
+            return;
+
+        lineRender = GetComponent<LineRenderer>();
+        lineRender.SetPosition(0, transform.position);
+        lineRender.SetPosition(1, Ball.transform.position);
+    }
+
+    private void Awake()
+    {
+        lineRender = GetComponent<LineRenderer>();
+        lineRender.SetPosition(0, transform.position);
+    }
+
+
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(Mathf.Sin(Time.time * speed) * amp, 0, 0));
+        lineRender.SetPosition(1, Ball.transform.position);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Ball == null)
+            return;
+
+        Gizmos.color = Color.red;        
+        Gizmos.DrawLine(transform.position, Ball.transform.position) ;
+    }
+
+  
+}
---
+++ Assets/Scripts/BallChainTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Events;
+
+public class BallChainTrigger : MonoBehaviour
+{
+    public UnityEvent CollideWithPlayer;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("OnTriggerEnter" + other);
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            CollideWithPlayer?.Invoke();
+        }
+    }
+
+    //public void OnCollisionStay(Collision collision)
+    //{
+    //    Debug.Log("OnTriggerEnter" + collision);
+
+    //}
+}
